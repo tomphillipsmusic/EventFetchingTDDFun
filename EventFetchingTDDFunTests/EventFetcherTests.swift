f6375a87cb711b3d407807ad1000ea786ee58b89
Tom Phillips <tomphillipsmusic@gmail.com>
2023-11-06 16:56:44 -0500

    If eventstore cannot connect, we throw an error on EventService creation

--- EventFetchingTDDFunTests/EventFetcherTests.swift
+++ EventFetchingTDDFunTests/EventFetcherTests.swift
@@ -10,10 +10,16 @@ import EventKit
 
 protocol EventStore {
     func events(between startDate: Date, and endDate: Date) -> [Event]
+    func connect() -> Bool
 }
 
 struct MockEventStore: EventStore {
+    func connect() -> Bool {
+        return willConnect
+    }
+    
     let scheduledEvents: [Event]
+    var willConnect: Bool = true
     
     func events(between startDate: Date, and endDate: Date) -> [Event] {
         scheduledEvents
@@ -23,8 +29,16 @@ struct MockEventStore: EventStore {
 class EventService {
     let store: EventStore
     
-    init(store: EventStore) {
-        self.store = store
+    enum Error: Swift.Error {
+        case noConnectionToStore
+    }
+    
+    init(store: EventStore) throws{
+        if store.connect() {
+            self.store = store
+        } else {
+            throw Error.noConnectionToStore
+        }
     }
     
     func fetchDates(between startDate: Date, and endDate: Date) -> [Event] {
@@ -45,7 +59,7 @@ final class EventFetcherTests: XCTestCase {
 
     func test_fetchEvents_returnsEmptyIfNoDatesScheduledInGivenRange() {
         let store = MockEventStore(scheduledEvents: [])
-        let sut = EventService(store: store)
+        let sut = try! EventService(store: store)
         let startDate = makeDate(hour: 8, minute: 0)
         let endDate = makeDate(hour: 12, minute: 0)
         
@@ -56,7 +70,7 @@ final class EventFetcherTests: XCTestCase {
     func test_fetchEvents_returnsAllScheduledEventsIfAllEventsAreInTheGivenDateRange() {
         let allScheduledEvents = allScheduledEvents()
         let store = MockEventStore(scheduledEvents: allScheduledEvents)
-        let sut = EventService(store: store)
+        let sut = try! EventService(store: store)
     
         let startDate = makeDate(hour: 8, minute: 0)
         let endDate = makeDate(hour: 12, minute: 0)
@@ -68,13 +82,20 @@ final class EventFetcherTests: XCTestCase {
     func test_fetchEvents_returnsOnlyScheduledEventsInTheGivenDateRange() {
         let allScheduledEvents = allScheduledEvents()
         let store = MockEventStore(scheduledEvents: allScheduledEvents)
-        let sut = EventService(store: store)
+        let sut = try! EventService(store: store)
     
         let startDate = makeDate(hour: 6, minute: 0)
         let endDate = makeDate(hour: 9, minute: 0)
         
         let scheduledEvents = sut.fetchDates(between: startDate, and: endDate)
         XCTAssertEqual(scheduledEvents, [allScheduledEvents[0]])
+    }
+    
+    // TODO: How can I cover more edge cases with fetching events from different ranges?
+    
+    func test_fetchEvents_throwsNoConnectionToEventStoreErrorIfEventsAreNotAbleToBeFetched() {
+        let store = MockEventStore(scheduledEvents: [], willConnect: false)
+        XCTAssertThrowsError(try EventService(store: store))
     }
     
     // MARK: Helper Methods

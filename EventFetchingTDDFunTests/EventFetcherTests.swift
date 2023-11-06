cf77b9ef49ff59f3aa8abe70e3d6bd5b3d741090
Tom Phillips <tomphillipsmusic@gmail.com>
2023-11-06 14:00:07 -0500

    changed EventFetcher to EventService to better capture its functinoality. Created a EventStore protocol and mock

--- EventFetchingTDDFunTests/EventFetcherTests.swift
+++ EventFetchingTDDFunTests/EventFetcherTests.swift
@@ -8,20 +8,40 @@
 import XCTest
 import EventKit
 
-class EventFetcher {
+protocol EventStore {
+    func events(between startDate: Date, and endDate: Date) -> [Event]
+}
+
+struct MockEventStore: EventStore {
+    let scheduledEvents: [Event]
+    
+    func events(between startDate: Date, and endDate: Date) -> [Event] {
+        scheduledEvents
+    }
+}
+
+class EventService {
+    let store: EventStore
+    
+    init(store: EventStore) {
+        self.store = store
+    }
+    
     func fetchDates(between startDate: Date, and endDate: Date) -> [Event] {
-        []
+        return store.events(between: startDate, and: endDate)
     }
 }
 
 struct Event: Equatable {
-    
+    let startDate: Date
+    let endDate: Date
 }
 
 final class EventFetcherTests: XCTestCase {
 
     func test_fetchEvents_returnsEmptyIfNoDatesScheduledInGivenRange() {
-        let sut = EventFetcher()
+        let store = MockEventStore(scheduledEvents: [])
+        let sut = EventService(store: store)
         let startDate = Calendar.autoupdatingCurrent.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!
         let endDate = Calendar.autoupdatingCurrent.date(bySettingHour: 12, minute: 0, second: 0, of: Date())!
         
@@ -29,14 +49,23 @@ final class EventFetcherTests: XCTestCase {
         XCTAssertEqual(scheduledEvents, [Event]())
     }
     
-//    func test_fetchEvents_returnsAllScheduledEventsInTheGivenDateRange() {
-//        let sut = EventFetcher()
-//        let startDate = Calendar.autoupdatingCurrent.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!
-//        let endDate = Calendar.autoupdatingCurrent.date(bySettingHour: 12, minute: 0, second: 0, of: Date())!
-//        
-//        let allScheduledEvents: [EKEvent] = EKEv
-//        
-//        let scheduledEvents = sut.fetchDates(between: startDate, and: endDate)
-//        XCTAssertEqual(scheduledEvents, [EKEvent]())
-//    }
+    func test_fetchEvents_returnsAllScheduledEventsInTheGivenDateRange() {
+        let firstEvent = Event(
+            startDate: Calendar.autoupdatingCurrent.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!,
+            endDate: Calendar.autoupdatingCurrent.date(bySettingHour: 8, minute: 30, second: 0, of: Date())!)
+        
+        let secondEvent = Event(
+            startDate: Calendar.autoupdatingCurrent.date(bySettingHour: 9, minute: 0, second: 0, of: Date())!,
+            endDate: Calendar.autoupdatingCurrent.date(bySettingHour: 9, minute: 30, second: 0, of: Date())!)
+        
+        
+        let allScheduledEvents: [Event] = [firstEvent, secondEvent]
+        let store = MockEventStore(scheduledEvents: allScheduledEvents)
+        let sut = EventService(store: store)
+        let startDate = Calendar.autoupdatingCurrent.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!
+        let endDate = Calendar.autoupdatingCurrent.date(bySettingHour: 12, minute: 0, second: 0, of: Date())!
+        
+        let scheduledEvents = sut.fetchDates(between: startDate, and: endDate)
+        XCTAssertEqual(scheduledEvents, allScheduledEvents)
+    }
 }

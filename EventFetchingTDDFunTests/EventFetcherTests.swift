e31135a3c103b7d2ceb3e2554d5fb689d4fd3225
Tom Phillips <tomphillipsmusic@gmail.com>
2023-11-06 14:22:00 -0500

    extracted some helper factory methods to get event generation out of test code

--- EventFetchingTDDFunTests/EventFetcherTests.swift
+++ EventFetchingTDDFunTests/EventFetcherTests.swift
@@ -50,22 +50,31 @@ final class EventFetcherTests: XCTestCase {
     }
     
     func test_fetchEvents_returnsAllScheduledEventsInTheGivenDateRange() {
-        let firstEvent = Event(
-            startDate: Calendar.autoupdatingCurrent.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!,
-            endDate: Calendar.autoupdatingCurrent.date(bySettingHour: 8, minute: 30, second: 0, of: Date())!)
-        
-        let secondEvent = Event(
-            startDate: Calendar.autoupdatingCurrent.date(bySettingHour: 9, minute: 0, second: 0, of: Date())!,
-            endDate: Calendar.autoupdatingCurrent.date(bySettingHour: 9, minute: 30, second: 0, of: Date())!)
-        
-        
-        let allScheduledEvents: [Event] = [firstEvent, secondEvent]
+        let allScheduledEvents = allScheduledEvents()
         let store = MockEventStore(scheduledEvents: allScheduledEvents)
         let sut = EventService(store: store)
+        
         let startDate = Calendar.autoupdatingCurrent.date(bySettingHour: 8, minute: 0, second: 0, of: Date())!
         let endDate = Calendar.autoupdatingCurrent.date(bySettingHour: 12, minute: 0, second: 0, of: Date())!
         
         let scheduledEvents = sut.fetchDates(between: startDate, and: endDate)
         XCTAssertEqual(scheduledEvents, allScheduledEvents)
+    }
+    
+    // MARK: Helper Methods
+    private func makeEvent(startDate: (hour: Int, minute: Int), endDate: (hour: Int, minute: Int)) -> Event {
+        Event(
+            startDate: Calendar.autoupdatingCurrent.date(bySettingHour: startDate.hour, minute: startDate.minute, second: 0, of: Date())!,
+            endDate: Calendar.autoupdatingCurrent.date(bySettingHour: endDate.hour, minute: endDate.minute, second: 0, of: Date())!
+        )
+    }
+    
+    private func allScheduledEvents() -> [Event] {
+        let firstEvent = makeEvent(startDate: (hour: 8, minute: 0), endDate: (hour: 8, minute: 30))
+        
+        let secondEvent = makeEvent(startDate: (hour: 9, minute: 0), endDate: (hour: 9, minute: 15))
+        
+        
+        return [firstEvent, secondEvent]
     }
 }
